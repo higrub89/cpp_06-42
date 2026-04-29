5513b9d28b8f3af2752c8dd8f322329eadc9b92d
higrub89 <higuitaruben@hotmail.com>
2026-04-29 23:49:44 +0200

    Finish project

--- ex02/Base.hpp
+++ ex02/Base.hpp
@@ -6,12 +6,14 @@
 /*   By: rhiguita <rhiguita@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/29 23:33:47 by rhiguita          #+#    #+#             */
-/*   Updated: 2026/04/29 23:33:48 by rhiguita         ###   ########.fr       */
+/*   Updated: 2026/04/29 23:46:14 by rhiguita         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BASE_HPP
 # define BASE_HPP
+
+#include <iostream>
 
 class Base {
 public:
--- ex02/functions.cpp
+++ ex02/functions.cpp
@@ -1,11 +1,21 @@
-#include <iostream>
-#include <cstdlib>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   functions.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rhiguita <rhiguita@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/29 23:43:31 by rhiguita          #+#    #+#             */
+/*   Updated: 2026/04/29 23:46:07 by rhiguita         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "functions.hpp"
 #include "A.hpp"
 #include "B.hpp"
 #include "C.hpp"
+#include <cstdlib>
 
-// Genera aleatoriamente una instancia de A, B o C
 Base* generate(void) {
 	int random = std::rand() % 3;
 	if (random == 0) {
@@ -20,8 +30,6 @@ Base* generate(void) {
 	}
 }
 
-// Identifica el tipo real usando PUNTERO
-// dynamic_cast con puntero retorna NULL si falla
 void identify(Base* p) {
 	if (dynamic_cast<A*>(p))
 		std::cout << "A" << std::endl;
@@ -33,8 +41,6 @@ void identify(Base* p) {
 		std::cout << "Unknown type" << std::endl;
 }
 
-// Identifica el tipo real usando REFERENCIA
-// dynamic_cast con referencia lanza std::bad_cast si falla
 void identify(Base& p) {
 	try {
 		(void)dynamic_cast<A&>(p);
